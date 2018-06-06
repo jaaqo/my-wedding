6b7a4df3550e760698932d35e4cd08185b044085
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2018-06-06 11:57:05 +0300

    Rewrite, simplify component tree to find bugs easier

--- src/cljs/my_wedding/pages/response.cljs
+++ src/cljs/my_wedding/pages/response.cljs
@@ -34,6 +34,12 @@
     (db/set-state assoc-in (into [:form] form-keys) (transformer-fn (.. event -target -value)))))
 
 
+(defn handle-checkbox-change [form-key checkbox-key]
+  (fn [event]
+    (let [prev-value (db/get-form-state form-key checkbox-key)]
+      (db/set-state assoc-in [:form form-key checkbox-key] (not prev-value)))))
+
+
 (defn handle-submit-success
   "Stop sending state and add a self-clearing notification of the form submit success."
   [response]
@@ -45,8 +51,7 @@
        :id id
        :type "success"
        :content (str "Henkilön "
-                     (get-in response [:data :firstName]) " "
-                     (get-in response [:data :lastName])
+                     (get-in response [:data :name]) " "
                      " ilmoittautuminen onnistui!"))
       (db/add-notification
        :id id
@@ -74,10 +79,8 @@
         errors (doall
                 (for [[k form-part] form]
                   (let [errors (cond-> {}
-                                 (str/blank? (:first-name form-part))
-                                 (assoc :first-name "Etunimi ei voi olla tyhjä")
-                                 (str/blank? (:last-name form-part))
-                                 (assoc :last-name "Sukunimi ei voi olla tyhjä"))]
+                                 (str/blank? (:name form-part))
+                                 (assoc :first-name "Nimi ei voi olla tyhjä"))]
                     (db/set-form-errors k errors))))]
     (if (every? empty? errors)
       form
@@ -93,18 +96,21 @@
     (when (js/confirm "Haluatko varmasti lähettää ilmoittautumislomakkeen?")
       (doall
        (for [[_ form-part] validated-form]
-         (let [url (db/get-state :api-url)
+         (let [url       (db/get-state :api-url)
                form-data (doto
                              (js/FormData.)
-                           (.append "firstName" (get form-part :first-name ""))
-                           (.append "lastName" (get form-part :last-name ""))
-                           (.append "allergies" (get form-part :allergies "")))]
+                           (.append "name" (get form-part :name ""))
+                           (.append "attending" (get form-part :attending ""))
+                           (.append "allergies" (get form-part :allergies ""))
+                           (.append "noTransportation" (get form-part :no-transportation "false"))
+                           (.append "transportationChurchVenue" (get form-part :transportation-church-venue "false"))
+                           (.append "transportationVenueCity" (get form-part :transportation-venue-city "false")))]
            (db/set-sending-state true)
-           (POST url {:body form-data
+           (POST url {:body            form-data
                       :response-format :json
-                      :keywords? true
-                      :handler handle-submit-success
-                      :error handle-submit-error})))))))
+                      :keywords?       true
+                      :handler         handle-submit-success
+                      :error           handle-submit-error})))))))
 
 (defn text-input-group [{:keys [name label form-key on-change value error]}]
   (let [err (error form-key)]
@@ -128,176 +134,155 @@
                :value     (value form-key)
                :on-change (on-change form-key)}]])
 
-(defn radio-input [{:keys [id name value label checked on-change]}]
-  [:div.form-check.form-check-inline
-   [:input.form-check-input {:type "radio"
-                             :name name
-                             :value value
-                             :id id
-                             :checked checked
-                             :on-change on-change}]
-   [:label.form-check-label {:for id}
-    label]])
-
 (defn checkbox-input [{:keys [id name value label checked on-change]}]
-  [:div.form-check
-   [:input.form-check-input {:type "checkbox"
-                             :name name
-                             :value value
-                             :id id
-                             :checked checked
-                             :on-change on-change}]
-   [:label.form-check-label {:for id}
-    label]])
-
-
-(defn response-form-fields [{:keys [key error value on-change checkbox-on-change checkbox-checked attending?]}]
-  [:div.fields
-   [:h3 (if (zero? key)
-          "Henkilö"
-          (str "Henkilö " (inc key))) ]
-
-   [text-input-group {:name "name"
-                      :label "Nimi:"
-                      :form-key :name
-                      :error error
-                      :value value
-                      :on-change on-change}]
-   (when attending?
-     [:div
-      [textarea-group {:name "allergies"
-                       :label "Erityisruokavalio/allergiat:"
-                       :form-key :allergies
-                       :error error
-                       :value value
-                       :on-change on-change}]
-      [:div.form-group
-       [:span "Osallistuminen yhteiskuljetukseen:"]
-       [checkbox-input {:label "En osallistu"
-                        :id "no-transportation"
-                        :value :no-transportation
-                        :checked (checkbox-checked :no-transportation)
-                        :on-change #(checkbox-on-change [key :no-transportation] %)}]
-       [checkbox-input {:label "Kirkolta juhlapaikalle"
-                        :id "transportation-church-venue"
-                        :value :transportation-church-venue
-                        :checked (checkbox-checked :transportation-church-venue)
-                        :on-change #(checkbox-on-change [key :transportation-church-venue] %)}]
-       [checkbox-input {:label "Juhlapaikalta Hyvinkään keskustaan"
-                        :id "transportation-venue-city"
-                        :value :transportation-venue-city
-                        :checked (checkbox-checked :transportation-venue-city)
-                        :on-change #(checkbox-on-change [key :transportation-venue-city] %)}]]])])
-
-
-(defn response-form [& {:keys [on-submit
-                               form
-                               error
-                               text-input-value
-                               radio-input-value
-                               on-text-input-change
-                               on-radio-input-change
-                               on-checkbox-input-change
-                               on-add-another
-                               on-clear
-                               on-remove-person
-                               on-captcha-verify
-                               on-captcha-expiry
-                               on-captcha-load
-                               checkbox-checked
-                               attending?
-                               sending?
-                               disabled?]}]
-  [:form.form {:id        "response-form"
-               :on-submit on-submit
-               :no-validate true}
-   [:div.form-group
-    [radio-input {:value :attending
-                  :id    "attending-check"
-                  :name  "attending"
-                  :label "Osallistun"
-                  :checked   (radio-input-value :attending)
-                  :on-change on-radio-input-change}]
-    [radio-input {:value :not-attending
-                  :id    "not-attending-check"
-                  :name  "attending"
-                  :label "En osallistu"
-                  :checked   (radio-input-value :not-attending)
-                  :on-change on-radio-input-change}]]
-   (doall
-    (for [[k _] form]
-      ^{:key k}
-      [:div.person
-       [response-form-fields {:key k
-                              :value (partial text-input-value k)
-                              :error (partial error k)
-                              :attending? attending?
-                              :checkbox-on-change on-checkbox-input-change
-                              :checkbox-checked (partial checkbox-checked k)
-                              :on-change (partial on-text-input-change k)}]
-       (when-not (zero? k)
-         [:button.btn.btn-light.btn-sm {:type "button"
-                                       :on-click (partial on-remove-person k)} (str "Poista henkilö " (inc k))])
-       [:hr]]))
-   (when attending?
-     [:div
-      [:button {:type     "button"
-                :on-click on-add-another
-                :class    "btn btn-secondary"}
-       "Lisää toinen henkilö"]
-      [:hr]])
-   [:div.form-group
-    [recaptcha {:sitekey         "6Ldd01sUAAAAAIFjlGRC2PvEbG36YYu45A6DFqZ8"
-                :hl              "fi"
-                :element-ID      "captcha"
-                :render          "explicit"
-                :onload-callback on-captcha-load
-                :expired-callback on-captcha-expiry
-                :verify-callback on-captcha-verify}]]
-   [:button {:type     "submit"
-             :class    "btn btn-primary"
-             :style {:margin-right "15px"}
-             :disabled disabled?}
-    (if sending?
-      "Lähetetään..."
-      "Lähetä")]
-   [:button {:type     "button"
-             :on-click on-clear
-             :class    "btn btn-dark"}
-      "Tyhjennä"]])
-
-(defn checkbox-on-change [form-keys event]
-  (let [previous-state (apply db/get-form-state form-keys)
-        new-value (.. event -target -value)]
-    (db/set-state assoc-in (into [:form] form-keys) (toggle-keyword previous-state new-value))))
+  )
 
 (defn response-form-container []
-  (let [captcha-ok (r/atom false)]
+  (let [captcha-ok                    (r/atom true)
+        default-form-key              0
+        attending-radio-checked       #(if (= (db/get-form-state default-form-key :attending)
+                                              :attending)
+                                         true
+                                         false)
+        checkbox-checked              (fn [form-key checkbox-key]
+                                        (let [prev-value (db/get-form-state form-key checkbox-key)]
+                                          (if prev-value
+                                            true
+                                            false)))
+        handle-attending-radio-change (handle-change-transforming keyword default-form-key :attending)
+        handle-text-input-change      #(handle-change %1 %2)
+        handle-remove-person          #(when (js/confirm "Haluatko varmasti poistaa henkilön lomakkeelta?")
+                                         (db/remove-person %1))
+        handle-add-person             db/add-new-person-fields
+        handle-clear-form             #(when (js/confirm "Haluatko varmasti tyhjentää lomakkeen?")
+                                         (db/clear-form))
+        handle-captcha-load           onload-callback
+        handle-captcha-expiry         #(reset! captcha-ok false)
+        handle-captcha-verify         #(reset! captcha-ok true)
+        get-form-error                #(db/get-form-error %1 %2)
+        get-form-value                #(db/get-form-state %1 %2)]
     (fn []
-      [response-form
-       :form (db/get-state :form)
-       :on-submit handle-submit
-       :error #(db/get-form-error %1 %2)
-       :text-input-value #(db/get-form-state %1 %2)
-       :radio-input-value #(when (= (db/get-form-state 0 :attending) %1)
-                             "checked")
-       :checkbox-checked #(when (= (db/get-form-state %1 %2) %2)
-                            "checked")
-       :on-text-input-change #(handle-change %1 %2)
-       :on-radio-input-change (handle-change-transforming keyword 0 :attending)
-       :on-checkbox-input-change checkbox-on-change
-       :on-add-another db/add-new-person-fields
-       :on-clear  #(when (js/confirm "Haluatko varmasti tyhjentää lomakkeen?")
-                     (db/clear-form))
-       :on-remove-person #(when (js/confirm "Haluatko varmasti poistaa henkilön lomakkeelta?")
-                            (db/remove-person %1))
-       :disabled? (or (db/get-state :sending)
-                      (not @captcha-ok))
-       :sending? (db/get-state :sending)
-       :attending? (= (db/get-form-state 0 :attending) :attending)
-       :on-captcha-load onload-callback
-       :on-captcha-expiry #(reset! captcha-ok false)
-       :on-captcha-verify #(reset! captcha-ok true)])))
+      (let [disabled?  (or (db/get-state :sending)
+                           (not @captcha-ok))
+            sending?   (db/get-state :sending)
+            attending? (= (db/get-form-state 0 :attending) :attending)
+            form       (db/get-state :form)]
+        [:form.form {:id          "response-form"
+                     :on-submit   handle-submit
+                     :no-validate true}
+         [:div.form-group
+          [:div.form-check.form-check-inline
+           [:input.form-check-input {:type      "radio"
+                                     :name      "attending"
+                                     :value     :attending
+                                     :id        "attending-check"
+                                     :checked   (attending-radio-checked)
+                                     :on-change handle-attending-radio-change}]
+           [:label.form-check-label {:for "attending-check"}
+            "Osallistun"]]
+
+          [:div.form-check.form-check-inline
+           [:input.form-check-input {:type      "radio"
+                                     :name      "attending"
+                                     :value     :not-attending
+                                     :id        "not-attending-check"
+                                     :checked   (not (attending-radio-checked))
+                                     :on-change handle-attending-radio-change}]
+           [:label.form-check-label {:for "not-attending-check"}
+            "En osallistu"]]]
+
+         (doall
+          (for [[key _] form]
+            (let [error     (partial get-form-error key)
+                  value     (partial get-form-value key)
+                  on-change (partial handle-text-input-change key)
+                  suffix    (fn [s]
+                              (str s "-" key))]
+              ^{:key key}
+              [:div.person
+               [:div.fields
+                [:h3 (if (zero? key)
+                       "Henkilö"
+                       (str "Henkilö " (inc key)))]
+                [text-input-group {:name      "name"
+                                   :label     "Nimi:"
+                                   :form-key  :name
+                                   :error     error
+                                   :value     value
+                                   :on-change on-change}]
+                (when attending?
+                  [:div
+                   [textarea-group {:name      "allergies"
+                                    :label     "Erityisruokavalio/allergiat:"
+                                    :form-key  :allergies
+                                    :error     error
+                                    :value     value
+                                    :on-change on-change}]
+                   [:div.form-group
+                    [:span "Osallistuminen yhteiskuljetukseen:"]
+                    [:div.form-check
+                     [:input.form-check-input {:type      "checkbox"
+                                               :id        (suffix "no-transportation")
+                                               :name      "no-transportation"
+                                               :checked   (checkbox-checked key :no-transportation)
+                                               :on-change (handle-checkbox-change key :no-transportation)}]
+                     [:label.form-check-label {:for (suffix "no-transportation")}
+                      "En osallistu"]]
+
+                    (let [disabled? (checkbox-checked key :no-transportation)]
+                      [:div
+                       [:div.form-check
+                        [:input.form-check-input {:type      "checkbox"
+                                                  :id        (suffix "transportation-church-venue")
+                                                  :name      "transportation-church-venue"
+                                                  :checked   (checkbox-checked key :transportation-church-venue)
+                                                  :disabled  disabled?
+                                                  :on-change (handle-checkbox-change key :transportation-church-venue)}]
+                        [:label.form-check-label {:for (suffix "transportation-church-venue")}
+                         "Kirkolta juhlapaikalle"]]
+                       [:div.form-check
+                        [:input.form-check-input {:type      "checkbox"
+                                                  :id        (suffix "transportation-venue-city")
+                                                  :name      "transportation-venue-city"
+                                                  :checked   (checkbox-checked key :transportation-venue-city)
+                                                  :disabled  disabled?
+                                                  :on-change (handle-checkbox-change key :transportation-venue-city)}]
+                        [:label.form-check-label {:for (suffix "transportation-venue-city")}
+                         "Juhlapaikalta Hyvinkään keskustaan"]]])]])]
+               (when-not (zero? key)
+                 [:button.btn.btn-light.btn-sm {:type     "button"
+                                                :on-click (partial handle-remove-person key)}
+                  (str "Poista henkilö " (inc key))])
+               [:hr]])))
+
+         (when attending?
+           [:div
+            [:button {:type     "button"
+                      :on-click handle-add-person
+                      :class    "btn btn-secondary"}
+             "Lisää toinen henkilö"]
+            [:hr]])
+
+         [:div.form-group
+          [recaptcha {:sitekey          "6Ldd01sUAAAAAIFjlGRC2PvEbG36YYu45A6DFqZ8"
+                      :hl               "fi"
+                      :element-ID       "captcha"
+                      :render           "explicit"
+                      :onload-callback  handle-captcha-load
+                      :expired-callback handle-captcha-expiry
+                      :verify-callback  handle-captcha-verify}]]
+
+         [:button {:type     "submit"
+                   :class    "btn btn-primary"
+                   :style    {:margin-right "15px"}
+                   :disabled disabled?}
+          (if sending?
+            "Lähetetään..."
+            "Lähetä")]
+         [:button {:type     "button"
+                   :on-click handle-clear-form
+                   :class    "btn btn-dark"}
+          "Tyhjennä"]]))))
 
 
 (defn response-page []
