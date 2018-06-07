3be1deae2b1690cfbfa4840020826055cc52f903
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2018-06-07 15:08:16 +0300

    Remove unnecessary dep

--- project.clj
+++ project.clj
@@ -15,7 +15,6 @@
                                                [cljsjs/react-dom]]]
 
 
-                 [cljsjs/google-map-react "0.22.0-0"]
                  [cljsjs/react-google-maps "9.4.5-0"]
                  [secretary "1.2.3"]
                  [venantius/accountant "0.2.4"]
